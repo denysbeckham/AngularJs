258414329177977a3f74c3c883b8b96c74a0a9a1
Denys Jorge Ribeiro de Lima <T912596@prodam.local>
2022-06-13 15:36:48 -0400

    Atualização de codigos para estudo do AngularJs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+// import { DesafioComponentComponent } from './components/desafio-component/desafio-component.component';
+import { ParentDataComponent } from './component/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule
